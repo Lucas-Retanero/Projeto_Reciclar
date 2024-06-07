f153efd67cf7e5375839f037feb3b0993ed25fea
Lucas-Retanero <glucas.raro@gmail.com>
2024-06-07 03:23:45 -0300

    nona commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,36 @@
+// botão menu
+
+function navbar() {
+    let menuMobile = document.querySelector('nav');
+
+    if (menuMobile.classList.contains('clicado')) {
+        menuMobile.classList.remove('clicado');
+        document.querySelector('.menu-hamburguer').src = "./src/icons/menu-hamburguer.png"
+    } else {
+        menuMobile.classList.add('clicado');
+        document.querySelector('.menu-hamburguer').src = "./src/icons/x.png"
+    }
+}
+
+
+
+
+
+
+// const navbar = document.querySelector('.menu').addEventListener('click', function()){
+//     navbar.classList.add('clicado');
+// }
+
+
+
+
+
+
+
+
+
+//botão topo
+
 const scrollAnima = document.querySelector('[data-anima="scroll"]');
 
 const metadeWindow = window.innerHeight * 4;
